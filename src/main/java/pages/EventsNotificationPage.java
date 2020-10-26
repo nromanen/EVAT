c2ca46c76af21201413ac348333b3fcd166082cf
Denys Bozhevilnyi <d.bozhevilnyi@gmail.com>
2020-10-26 17:06:47 +0200

    added EventsNotificationPage

--- src/main/java/pages/EventsNotificationPage.java
+++ src/main/java/pages/EventsNotificationPage.java
@@ -6,28 +6,73 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class EventsNotificationPage {
-	
-private WebDriver driver;
-	
+
+	private WebDriver driver;
+
 	public EventsNotificationPage(WebDriver webDriver) {
 		this.driver = webDriver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(css = "#main > div > div > div > div.MuiCardActions-root > div > div.d-flex.flex-row.align-items-center.justify-content-center.float-right > a > button > span.MuiTouchRipple-root")
 	WebElement viewEventButton;
-	
+
 	@FindBy(css = "#main > div > div > div > div.MuiCardActions-root > div > div.d-flex.flex-row.align-items-center.justify-content-center.float-right > div > button")
 	WebElement shareEventButton;
+
+	@FindBy(css = "#customized-menu > div.MuiPaper-root.MuiMenu-paper.jss1680.MuiPaper-elevation0.MuiPopover-paper.MuiPaper-rounded > ul > li > button")
+	WebElement shareOnFaceBookButton;
+	
+	@FindBy(css = "#customized-menu > div.MuiPaper-root.MuiMenu-paper.jss1680.MuiPaper-elevation0.MuiPopover-paper.MuiPaper-rounded > ul > li > a:nth-child(2)")
+	WebElement shareOnTelegramButton;
+	
+	@FindBy(css = "#customized-menu > div.MuiPaper-root.MuiMenu-paper.jss1680.MuiPaper-elevation0.MuiPopover-paper.MuiPaper-rounded > ul > li > a:nth-child(3)")
+	WebElement shareOnTwiterButton;
+	
+	@FindBy(css = "#customized-menu > div.MuiPaper-root.MuiMenu-paper.jss1680.MuiPaper-elevation0.MuiPopover-paper.MuiPaper-rounded > ul > li > a:nth-child(4)")
+	WebElement shareOnLinkedInButton;
 	
 	public void clickOnViewEventButton() {
 		viewEventButton.click();
 	}
-	
+
 	public void clickOnShareEventButton() {
 		shareEventButton.click();
 	}
-	
-	// TODO
 
+	public void clickOnShareOnFaceBookButton() {
+		shareOnFaceBookButton.click();
+	}
+	
+	public void clickOnShareOnTelegramButton() {
+		shareOnTelegramButton.click();
+	}
+	
+	public void clickOnShareOnTwiterButton() {
+		shareOnTwiterButton.click();
+	}
+	
+	public void clickOnShareOnLinkedInButton() {
+		shareOnLinkedInButton.click();
+	}
+	
+	public void shareEventOnFaceBook() {
+		clickOnShareEventButton();
+		clickOnShareOnFaceBookButton();
+	}
+	
+	public void shareEventOnTelegram() {
+		clickOnShareEventButton();
+		clickOnShareOnTelegramButton();
+	}
+	
+	public void shareEventOnTwiter() {
+		clickOnShareEventButton();
+		clickOnShareOnTwiterButton();
+	}
+	
+	public void shareEventOnLinkedIn() {
+		clickOnShareEventButton();
+		clickOnShareOnLinkedInButton();
+	}
 }
