a0bbb96614e7d523b24b523468d1b5f96ce757c5
YuliaBodniak <myshzlotech@gmail.com>
2020-10-28 09:12:20 +0200

    change queries for SearchRepository

--- src/main/java/jdbc/SearchRepository.java
+++ src/main/java/jdbc/SearchRepository.java
@@ -18,61 +18,51 @@ public class SearchRepository {
     }
 
     public static int getNumberOfEventsWithKeywordInTitleOrDescriptionWithNInQuery(String keyword) throws SQLException {
-        String query = "select count(Id) from [eventsexpress-test].[dbo].[Events] where Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%'";
+        String query = "select COUNT(Id) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] where Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsWithTwoDates(LocalDate date1, LocalDate date2) throws SQLException {
-        String query = "select count(Id) from [eventsexpress-test].[dbo].[Events] where DateFrom >= '" + date1 + "' AND DateTo <= '" + date2 + "'";
+        String query = "select COUNT(Id) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] where DateFrom >= '" +date1+ "' AND DateTo <= '" +date2+ "' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsWithDateFrom(LocalDate date) throws SQLException {
-        String query = "select count(Id) from [eventsexpress-test].[dbo].[Events] where DateFrom >= '" + date + "'";
+        String query = "select COUNT(Id) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] where DateFrom >= '"+date+ "' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsFromNowTillDateTo(LocalDate date) throws SQLException {
         LocalDate date1 = LocalDate.now();
-        String query = "select count(Id) from [eventsexpress-test].[dbo].[Events] where DateFrom >= '" + date1 + "' AND DateTo <= '" + date + "'";
+        String query = "select COUNT(Id) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] where DateFrom >= '" +date1+ "' AND DateTo <= '"+date+ "' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
-    public static String getLocalDateNow() {
+    public static String getLocalDateNow(){
         LocalDate date = LocalDate.now();
-        return date.getMonthValue() + "/" + date.getDayOfMonth() + "/" + date.getYear();
+        return date.getMonthValue()+"/"+date.getDayOfMonth()+"/"+date.getYear();
     }
-
     public static int getNumberOfEventsByOneHashtag(String hashtag) throws SQLException {
         LocalDate date = LocalDate.now();
-        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId where Name = '" + hashtag + "' and DateFrom >= '" + date + "'";
+        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId where Name = '" +hashtag+ "' and DateFrom >= '"+date+"' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsByTwoHashtags(String hashtag1, String hashtag2) throws SQLException {
         LocalDate date = LocalDate.now();
-        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId where Name = '" + hashtag1 + "' and DateFrom >= '" + date + "' or Name = '" + hashtag2 + "' and DateFrom >= '" + date + "'";
+        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId where Name = '" +hashtag1+ "' and DateFrom >= '"+date+"' or Name = '"+hashtag2+"' and DateFrom >= '"+date+"' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsWithKeywordWithNInQueryAndDateFrom(String keyword, LocalDate date) throws SQLException {
-        String query = "select count(Id) from [eventsexpress-test].[dbo].[Events] where Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND DateFrom >= '" + date + "'";
+        String query = "select COUNT(Id) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] where Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND DateFrom >= '"+date+ "' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsByKeywordAndHashtag(String keyword, String hashtag) throws SQLException {
         LocalDate date = LocalDate.now();
-        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId WHERE Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND Name = '" + hashtag + "' and DateFrom >= '" + date + "'";
+        String query = "select COUNT(Name) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId WHERE Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND Name = '" +hashtag+ "' and DateFrom >= '"+date+"' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsByDatesAndHashtag(LocalDate date1, LocalDate date2, String hashtag) throws SQLException {
-        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId WHERE DateFrom >= '" + date1 + "' AND DateTo <= '" + date2 + "'AND Name = '" + hashtag + "'";
+        String query = "select COUNT(Name) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId WHERE DateFrom >= '" +date1+ "' AND DateTo <= '"+date2+ "'AND Name = '" +hashtag+ "' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
-
     public static int getNumberOfEventsByKeywordWithNInQueryAndDateFromAndHashtag(String keyword, LocalDate date, String hashtag) throws SQLException {
-        String query = "select COUNT(Name) from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId WHERE Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND Name = '" + hashtag + "' and DateFrom >= '" + date + "'";
+        String query = "select COUNT(Name) from (select TOP(6) * from [eventsexpress-test].[dbo].[Events] JOIN [eventsexpress-test].[dbo].[EventCategory] ON Events.Id = EventCategory.EventId JOIN [eventsexpress-test].[dbo].[Categories] ON Categories.Id = EventCategory.CategoryId WHERE Title LIKE N'%" + keyword + "%' OR Description LIKE N'%" + keyword + "%' AND Name = '" +hashtag+ "' and DateFrom >= '"+date+"' AND IsBlocked = 'false') a";
         return getNumberOfFoundedEvents(query);
     }
 }
