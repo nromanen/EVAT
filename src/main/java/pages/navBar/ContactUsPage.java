0dbc7d250ecae901e9dab862b53eff5ecfab62ba
YuliaBodniak <myshzlotech@gmail.com>
2020-10-28 19:36:57 +0200

    Add drop down list in ContactUsPage

--- src/main/java/pages/navBar/ContactUsPage.java
+++ src/main/java/pages/navBar/ContactUsPage.java
@@ -11,10 +11,21 @@ public class ContactUsPage {
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
-    @FindBy(css = ".form-control")
-    public WebElement problemType;
 
-//  drop-down list
+    @FindBy(css = ".form-control")
+    public WebElement problemTypeDropDownList;
+
+    @FindBy(css = ".form-control > option:nth-child(2)")
+    public WebElement newCategoryProblem;
+
+    @FindBy(css = ".form-control > option:nth-child(3)")
+    public WebElement bugReportProblem;
+
+    @FindBy(css = ".form-control > option:nth-child(4)")
+    public WebElement badEventProblem;
+
+    @FindBy(css = ".form-control > option:nth-child(5)")
+    public WebElement badUserProblem;
 
     @FindBy(css = ".MuiInputBase-input")
     public WebElement descriptionField;
@@ -28,13 +39,28 @@ public class ContactUsPage {
     @FindBy(css = "button.MuiButtonBase-root:nth-child(3)")
     public WebElement submitButton;
 
-    public ContactUsPage clickOnProblemType(){
-        problemType.click();
+    public ContactUsPage clickOnProblemTypeDropDownList(){
+        problemTypeDropDownList.click();
         return this;
     }
-//    public ContactUsPage selectProblemType(){
-//
-//    }
+    public ContactUsPage selectProblemType(String problem){
+        clickOnProblemTypeDropDownList();
+        switch (problem) {
+            case ("New Category"):
+                newCategoryProblem.click();
+                break;
+            case ("Bug Report"):
+                bugReportProblem.click();
+                break;
+            case ("Bad Event"):
+                badEventProblem.click();
+                break;
+            case ("Bad User"):
+                badUserProblem.click();
+                break;
+        }
+        return this;
+    }
     public ContactUsPage enterDescription(String text){
         descriptionField.sendKeys(text);
         return this;
