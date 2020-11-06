1529828bd782f791df5083c8f460915a78e9013f
Antialtruist <d.bozhevilnyi@gmail.com>
2020-11-06 13:11:56 +0200

    fixed EventInfoPageTest

--- src/test/java/profile/EventInfoPageTest.java
+++ src/test/java/profile/EventInfoPageTest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.EventInfoPage;
+import pages.SignInUpMenu;
 import utility.SetUpDriver;
 
 public class EventInfoPageTest {
@@ -22,12 +23,11 @@ public class EventInfoPageTest {
 	private WebDriverWait wait;
 	private SetUpDriver setUpDriver;
 	private EventInfoPage eventInfoPage;
+	private SignInUpMenu signInUpMenu;
 	
-	private String url = "https://eventsexpress.azurewebsites.net/home/events?page=1";
-	private By signInUpButton = By.xpath("//span[contains(text(),'Sign In/Up')]");
-	private By email = By.name("email");
-	private By password = By.name("password");
-	private By signin = By.cssSelector(".MuiDialogActions-root > button:nth-child(2)");
+	private String url = "https://eventsexpress-test.azurewebsites.net/home/events?page=1";
+	private String email = "d.bozhevilnyi@gmail.com";
+	private String pass = "131089";
 	private By eventInfoButton = By.xpath("//*[@id=\"main\"]/div[2]/div/div[2]/div/div[4]/div/div[3]/a/button");
 	private String comment = "auto generated comment";
 
@@ -37,6 +37,7 @@ public class EventInfoPageTest {
 		driver = setUpDriver.getDriver();
 		wait = setUpDriver.getWebDriverWait();
 		eventInfoPage = PageFactory.initElements(driver, EventInfoPage.class);
+		signInUpMenu = PageFactory.initElements(driver, SignInUpMenu.class);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
@@ -44,10 +45,7 @@ public class EventInfoPageTest {
 	@BeforeClass
 	public void profileSetup() {
 		driver.get(url);
-		driver.findElement(signInUpButton).click();
-		driver.findElement(email).sendKeys("d.bozhevilnyi@gmail.com");
-		driver.findElement(password).sendKeys("131089");
-		driver.findElement(signin).click();
+		signInUpMenu.authoriseUser(email,pass);
 		wait.until(ExpectedConditions.elementToBeClickable(eventInfoButton));
 		driver.findElement(eventInfoButton).click();
 	}
