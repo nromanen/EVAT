9d1f417df1589a69be132536ec543f0fad9a16f6
GulpakMarina <gulpakmarina@gmail.com>
2020-11-29 23:22:22 +0200

    test first

--- src/test/java/profile/UserInfoPageTest.java
+++ src/test/java/profile/UserInfoPageTest.java
@@ -1,5 +1,6 @@
 package profile;
 
+import io.qameta.allure.Description;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -27,7 +28,7 @@ public class UserInfoPageTest extends ProfileBaseTest{
      * @param gender
      * @param interests
      */
-
+    @Description("Verify information on the page profile")
     @Test(dataProvider = "providerEmail")
     public void testGetValueUserName(String email,String password, String userName, String age,
                                      String gender, String interests) {
